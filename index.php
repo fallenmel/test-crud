3439b2f5312c4f296c4dc63b589515308725401a
DNY.UNLTD <luceriodony@gmail.com>
2018-09-23 23:21:49 +0800

    setup php template

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+	<head>
+
+		<!-- meta's -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+		<meta name="robots" content="index, follow">
+
+		<title>HOME PAGE TITLE</title>
+
+
+		<!-- fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins:400,700,900" rel="stylesheet">
+
+		<!-- stylesheet -->
+		<link rel="stylesheet" type="text/css" href="./assets/css/common.css">
+
+	</head>
+	<body>
+		<div class="siteContainer">	
+			<header>
+				<div class="logoWrap">
+					<!-- <img src="./assets/images/logo.png"> -->
+				</div>
+				<nav>
+					<ul>
+						<li><a href="./">HOME</a></li>
+						<li><a href="./product">PRODUCT</a></li>
+						<li><a href="./opportunities">OPPORTUNITIES</a></li>
+						<li><a href="./more">MORE</a></li>
+					</ul>
+				</nav>
+			</header>
+
+			<!-- banner -->
+			<div class="banner">				
+				<video autoplay loop muted>
+					<source src="./assets/video/bannervid.mp4" type="video/mp4">
+				</video>
+			</div>
+
+			<main>
+				<!-- add content here -->
+			</main>
+		</div>
+	</body>
+</html>	
---
+++ product/index.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+
+		<!-- meta's -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+		<meta name="robots" content="index, follow">
+
+		<title>Product TITLE</title>
+
+		<!-- stylesheet -->
+		<link rel="stylesheet" type="text/css" href="../assets/css/common.css">
+
+	</head>
+	<body>
+		<div class="siteContainer">		
+			<header>
+<!-- 				<div class="logoWrap">
+					<img src="../assets/images/logo.png">
+				</div> -->
+				<nav>
+					<ul>
+						<li><a href="../">HOME</a></li>
+						<li><a href="../product">PRODUCT</a></li>
+						<li><a href="../opportunities">OPPORTUNITIES</a></li>
+						<li><a href="../more">MORE</a></li>
+					</ul>
+				</nav>
+			</header>
+			<!-- banner -->
+			<div class="banner">				
+				<video autoplay>
+					<source src="./assets/video/bannervid.mpd" type="video/mp4">
+				</video>
+			</div>
+
+			<main>
+				<!-- add content here -->
+			</main>
+
+		</div>
+	</body>
+</html>	
